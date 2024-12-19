52c2ae7425a7bb32de80418ccb75bca0beadd7ea
Pablo López Luján <82211277+pablolop12@users.noreply.github.com>
2024-12-19 18:52:42 +0100

    Update CorsConfig.java

--- src/main/java/com/pablo/nutritional_tracker/config/CorsConfig.java
+++ src/main/java/com/pablo/nutritional_tracker/config/CorsConfig.java
@@ -8,11 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") 
-                .allowedOrigins("https://nutriweb.pablolopezlujan.es") // URL Frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*") // Headers permitidos
-                .allowCredentials(true);
-    }
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("https://nutriweb.pablolopezlujan.es")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        }
 }
